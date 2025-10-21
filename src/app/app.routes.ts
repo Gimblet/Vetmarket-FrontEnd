a86ce99806d16ae0cd3844d15da85ed650a5d54b
Christhian <peralta6998@gmail.com>
2025-10-21 01:13:02 -0500

    Ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import {Routes} from '@angular/router';
 import {ProductoComponent} from './producto/producto.component'
 import {LoginComponent} from './auth/login/login.component';
-import {HomeComponent} from './cliente/home/home.component';
 import {DetallesComponent} from './producto/detalles/detalles.component';
 
 export const routes: Routes = [
-  // Ruta inicial
-  { path: '', component: HomeComponent },
+
   // Auth
   { path: 'login', component: LoginComponent },
-  // Producto
+  // Producto -- Producto sera la ruta inicial
   {
     path: 'producto',
     children: [
