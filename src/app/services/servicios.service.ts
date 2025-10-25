08bd0294d52dc7be6518895be341ae8f549bda23
Dante-Diaz-20 <zevaztian.20@gmail.com>
2025-10-24 23:43:09 -0500

    feat:add implement servicio view (admin)100%

--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -19,13 +19,13 @@ return this.httpClient.get<ServicioResponseDTO>(`${this.apiUrl}/${id}`)
 
 }
 
-  agregarServicio(servicio: ServicioRequestDTO): Observable<ServicioResponseDTO> {
+  /*agregarServicio(servicio: ServicioRequestDTO): Observable<ServicioResponseDTO> {
     return this.httpClient.post<ServicioResponseDTO>(`${this.apiUrl}`, servicio)
   }
 
   editarServicio(id: number, servicio: ServicioRequestDTO): Observable<ServicioResponseDTO> {
     return this.httpClient.put<ServicioResponseDTO>(`${this.apiUrl}/${id}`, servicio)
-  }
+  }  */
 
   eliminarServicio(id:number): Observable<void>{
     return this.httpClient.delete<void>(`${this.apiUrl}/${id}`)
@@ -34,5 +34,15 @@ return this.httpClient.get<ServicioResponseDTO>(`${this.apiUrl}/${id}`)
   obtenerImagen(id: number): Observable<Blob> {
   return this.httpClient.get(`${this.apiUrl}/imagen/${id}`, { responseType: 'blob' });
 }
+
+// NUEVO: para crear con imagen
+  agregarServicioConImagen(formData: FormData): Observable<ServicioResponseDTO> {
+    return this.httpClient.post<ServicioResponseDTO>(this.apiUrl, formData);
+  }
+
+  // NUEVO: para editar con imagen
+  editarServicioConImagen(id: number, formData: FormData): Observable<ServicioResponseDTO> {
+    return this.httpClient.put<ServicioResponseDTO>(`${this.apiUrl}/${id}`, formData);
+  }
 
 }
