ac46082ae32af6b0ed72d7ad41dd096d3ba611ea
Gimblet <gimbletdev@gmail.com>
2025-10-31 17:10:01 -0500

    Reverted cita service

--- src/app/services/cita.service.ts
+++ src/app/services/cita.service.ts
@@ -1,29 +1,25 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { DetalleDto } from '../interface/ServicioCita/DetalleDto';
-import {map, Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import {apiResponse} from '../interface/ApiResponse/api-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CitaService {
 
-  private apiUrl = environment.gatewayUrl+'/serviciocita';
+  private apiUrl = environment.gatewayUrl+'/serviciocita'; 
 
   constructor(private http: HttpClient) { }
 
-  nuevaCita(token: string | null, idUsuario: number, idServicio: number,
+  nuevaCita(token: string | null, idUsuario: number, idServicio: number, 
     idMascota: number, fechaCita: string):Observable<DetalleDto> {
-
+    
     const headers = new HttpHeaders({
       'Authorization': 'Bearer '+token
     });
 
-    return this.http
-      .get<apiResponse>(this.apiUrl+'/cita/nuevo', { headers, params:{idUsuario,idServicio,idMascota,fechaCita} })
-      .pipe(map((res) => (res?.data as DetalleDto) ?? []));
-
+    return this.http.get<DetalleDto>(this.apiUrl+'/cita/nuevo', { headers, params:{idUsuario,idServicio,idMascota,fechaCita} });
   }
 }
